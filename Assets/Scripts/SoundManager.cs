2894d625fe7aea29a624e7e752bd007b45a9940e
Sofhia <isabestil@gmail.com>
2023-01-08 18:08:54 +0800

    File upload
    Volume is adjustable

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetInt("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetInt("musicvolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+
+}
