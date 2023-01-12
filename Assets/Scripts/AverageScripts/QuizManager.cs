46ce242b764700d48a9381a36c797d9a4a4cf42c
Loanne <58616743+raine05@users.noreply.github.com>
2023-01-12 17:07:41 +0800

    Added the Average Game File

---
+++ Assets/Scripts/AverageScripts/QuizManager.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizManager : MonoBehaviour
+{
+    [SerializeField] private QuizUI quizUI;
+    [SerializeField] private List<QuizDataScriptable> quizData;
+    [SerializeField] private float timeLimit = 30f;
+
+    private List<Question> questions;
+
+    //current question data
+    private Question selectedQuestion;
+
+    private int scoreCount = 0;
+    private float currentTime;
+    private int lifeRemaining = 3;
+
+    private GameStatus gameStatus = GameStatus.Next;
+
+    public GameStatus GameStatus { get { return gameStatus; } }
+
+    // Start is called before the first frame update
+    public void StartGame(int index)
+    {
+        scoreCount = 0;
+        currentTime = timeLimit;
+        lifeRemaining = 3;
+        questions = new List<Question>();
+
+        for (int i = 0; i < quizData[index].questions.Count; i++)
+        {
+            //set the questions data
+            questions.Add(quizData[index].questions[i]);
+        }
+
+        SelectQuestion();
+        gameStatus = GameStatus.Playing;
+    }
+
+    void SelectQuestion()
+    {
+        int val = UnityEngine.Random.Range(0, questions.Count);
+        selectedQuestion = questions[val];
+
+        quizUI.SetQuestion(selectedQuestion);
+
+        questions.RemoveAt(val);
+    }
+
+    private void Update()
+    {
+        if (gameStatus == GameStatus.Playing)
+        {
+            currentTime -= Time.deltaTime;
+            SetTimer(currentTime);
+        }
+    }
+
+    private void SetTimer(float value)
+    {
+        TimeSpan time = TimeSpan.FromSeconds(value); //set the time value
+        quizUI.TimerText.text = time.ToString("mm':'ss"); //convert time to Time format
+
+        if (currentTime <= 0)
+        {
+            //Game Over
+            gameStatus = GameStatus.Next;
+            quizUI.GameOverPanel.SetActive(true);
+        }
+    }
+
+    public bool Answer(string answered)
+    {
+        bool correctAns = false;
+
+        if (answered == selectedQuestion.correctAns)
+        {
+            //Yes
+            correctAns = true;
+            scoreCount += 1;
+            quizUI.ScoreText.text = scoreCount + "/10";
+            
+        }
+        else
+        {
+            //No
+            //lifeRemaining--;
+            //quizUI.ReduceLife(lifeRemaining);
+
+            if (lifeRemaining <= 0)
+            {
+                gameStatus = GameStatus.Next;
+                quizUI.GameOverPanel.SetActive(true);
+            }
+        }
+
+        if (gameStatus == GameStatus.Playing)
+        {
+            if (questions.Count > 0)
+            {
+                //call SelectQuestion method again after 1s
+                Invoke("SelectQuestion", 0.4f);
+            }
+            else
+            {
+                gameStatus = GameStatus.Next;
+                quizUI.GameOverPanel.SetActive(true);
+            }
+
+        }
+
+
+        //Test
+        if (scoreCount == 10)
+        {
+            quizUI.star3.gameObject.SetActive(true);
+        }
+        else if (scoreCount > 4 && scoreCount < 10)
+        {
+            quizUI.star2.gameObject.SetActive(true);
+        }
+        else if (scoreCount == 0)
+        {
+            quizUI.star0.gameObject.SetActive(true);
+        }
+        else if (scoreCount < 5)
+        {
+            quizUI.star1.gameObject.SetActive(true);
+        }
+
+        //Test
+
+        //return the value of correct bool
+        return correctAns;
+    }
+}
+
+[System.Serializable]
+public class Question
+{
+    public string questionInfo; //question text
+    public QuestionType questionType;  //type
+    public Sprite questionImg; //image for Image Type
+    public AudioClip questionClip; //audio for audio type
+    public UnityEngine.Video.VideoClip questionVideo; //video for video type
+    public List<string> options; //options to select
+    public string correctAns; //correct option
+}
+
+[System.Serializable]
+public enum QuestionType
+{
+    TEXT,
+    IMAGE,
+    VIDEO,
+    AUDIO
+}
+
+[System.Serializable]
+public enum GameStatus
+{
+    Next,
+    Playing
+}
---
+++ Assets/Scripts/AverageScripts/QuizUI.cs
@@ -0,0 +1,131 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuizUI : MonoBehaviour
+{
+    [SerializeField] private QuizManager quizManager;
+    [SerializeField] private TextMeshProUGUI questionText, scoreText, timerText;
+    [SerializeField] private List<Image> lifeImageList;
+    [SerializeField] private GameObject gameOverPanel, averageMenuPanel, gameMenuPanel;
+    [SerializeField] public  GameObject star0, star1, star2, star3;
+    [SerializeField] private Image questionImage;
+    [SerializeField] private UnityEngine.Video.VideoPlayer questionVideo;
+    [SerializeField] private AudioSource questionAudio;
+    [SerializeField] private List<Button> options, uiButtons;
+    [SerializeField] private Color correctCol, wrongCol, normalCol;
+
+    private Question question;
+    private bool answered;
+    private float audioLength;
+
+    public TextMeshProUGUI ScoreText { get { return scoreText; } }
+
+    public TextMeshProUGUI TimerText { get { return timerText; } }
+    //public TextMeshProUGUI QuestionNum { get { return questionNum; } }
+
+    public GameObject GameOverPanel { get { return gameOverPanel; } }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for (int i = 0; i < options.Count; i++)
+        {
+            Button localBtn = options[i];
+            localBtn.onClick.AddListener(() => OnClick(localBtn));
+        }
+
+        for (int i = 0; i < uiButtons.Count; i++)
+        {
+            Button localBtn = uiButtons[i];
+            localBtn.onClick.AddListener(() => OnClick(localBtn));
+        }
+    }
+
+    public void SetQuestion(Question question)
+    {
+        this.question = question;
+
+        questionText.text = question.questionInfo;
+
+        List<string> answerList = ShuffleList.ShuffleListItems<string>(question.options);
+
+        for (int i = 0; i < options.Count; i++)
+        {
+            //set the child text
+            options[i].GetComponentInChildren<TextMeshProUGUI>().text = answerList[i];
+            options[i].name = answerList[i]; //set the name of button
+            options[i].image.color = normalCol; //set color of button to normal
+        }
+
+        answered = false;
+
+    }
+
+
+    void ImageHolder()
+    {
+        questionImage.transform.parent.gameObject.SetActive(true);
+        questionImage.transform.gameObject.SetActive(false);
+        questionAudio.transform.gameObject.SetActive(false);
+        questionVideo.transform.gameObject.SetActive(false);
+    }
+
+    private void OnClick(Button btn)
+    {
+        if (quizManager.GameStatus == GameStatus.Playing)
+        {
+            if (!answered)
+            {
+                answered = true;
+                bool val = quizManager.Answer(btn.name);
+
+                if (val)
+                {
+                    btn.image.color = correctCol;
+                }
+                else
+                {
+                    btn.image.color = wrongCol;
+                }
+            }
+        }
+
+        switch (btn.name)
+        {
+            case "Days":
+                quizManager.StartGame(0);
+                averageMenuPanel.SetActive(false);
+                gameMenuPanel.SetActive(true);
+                break;
+            case "Months":
+                quizManager.StartGame(1);
+                averageMenuPanel.SetActive(false);
+                gameMenuPanel.SetActive(true);
+                break;
+            /*case "Mix":
+                quizManager.StartGame(2);
+                mainMenuPanel.SetActive(false);
+                gameMenuPanel.SetActive(true);
+                break;*/
+            case "AverageDataQuestion":
+                quizManager.StartGame(2);
+                averageMenuPanel.SetActive(false);
+                gameMenuPanel.SetActive(true);
+                break;
+        }
+    }
+
+    public void RetryButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    /*public void ReduceLife(int index)
+    {
+        lifeImageList[index].color = wrongCol;
+    }*/
+}
