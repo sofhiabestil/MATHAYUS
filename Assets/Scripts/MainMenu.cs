6119cd4d8ad7f3d90825fd6b316bce0526e02c68
Sofhia <isabestil@gmail.com>
2023-02-02 21:40:29 +0800

    update

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,10 @@
+/*
+ * Main Menu
+ * - is for exiting the game application
+ * - 
+ * @Sofhia Bestil
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +12,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // To close the game
-    public void QuitGame() //the function should set as public to make it visble on the function section of MainMenu
+    /**To close the game
+    - the method should set as public to make it visble 
+    on the function section of MainMenu*/
+    public void QuitGame() 
     {
         Debug.Log("Quit on the Game!");
         Application.Quit();
-    }
-
-    
+    } 
 }
