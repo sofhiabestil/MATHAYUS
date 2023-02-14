353c58a215d78a5df62cabb2111472734b81d0fa
ROLINE JASON DIGAL <Rjaaaydigal@gmail.com>
2023-02-14 23:53:25 +0800

    Instruction Animation - Practice

--- Assets/Scripts/ButtonActivator.cs
+++ Assets/Scripts/ButtonActivator.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ButtonActivator : MonoBehaviour
 {
     public Button[] buttons;
+    public TextMeshProUGUI[] texts;
     public int activeButtonIndex = 0;
-
+    public int activeTextIndex = 0;
 
     public void ActivateNextButton()
     {
@@ -16,5 +18,12 @@ public class ButtonActivator : MonoBehaviour
         activeButtonIndex = (activeButtonIndex + 1) % buttons.Length;
         buttons[activeButtonIndex].gameObject.SetActive(true);
         buttons[activeButtonIndex].interactable = true;
+    }
+
+    public void ActivateNextText()
+    {
+        texts[activeTextIndex].gameObject.SetActive(false);
+        activeTextIndex = (activeTextIndex + 1) % texts.Length;
+        texts[activeTextIndex].gameObject.SetActive(true);
     }
 }
