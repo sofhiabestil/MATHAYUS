274ecd270dc970d1c73da3cbb214646bf01b448b
ROLINE JASON DIGAL <Rjaaaydigal@gmail.com>
2023-02-25 22:56:11 +0800

    Easy Instruction Update

--- Assets/Scripts/EasyScripts/SlotHolder.cs
+++ Assets/Scripts/EasyScripts/SlotHolder.cs
@@ -13,17 +13,28 @@ public class SlotHolder : MonoBehaviour, IDropHandler
     public Button checkButton;
     private bool allHoldersFilled = false;
     public bool filled = false;
- 
+    public Text EasyScoreText;
+
+    [SerializeField] private GameObject weekpanel, monthpanel1, monthpanel2, gameoverpanel;
+
+    public GameObject GameOverPanel { get { return gameoverpanel; } }
+
+    public GameObject MonthPanel1 { get { return monthpanel1; } }
+    
+    public GameObject MonthPanel2 { get { return monthpanel2; } }
+
+    public GameObject WeekPanel { get { return weekpanel; } }
+
+
     public void OnDrop(PointerEventData eventData)
     {
+       
         if (eventData.pointerDrag != null)
         {
             if (eventData.pointerDrag.GetComponent<DragAndDrop>().id == id)
             {
                 Debug.Log("Correct");
                 
-                score ++;
-                //EasyScoreText.text = score + "/10";
             }
             else
             {
@@ -33,27 +44,32 @@ public class SlotHolder : MonoBehaviour, IDropHandler
             filled = true;
         }
         checkAnswer();
-    }
 
+    }
     void Start()
     {
         checkButton.interactable = false;
     }
-
     public void checkAnswer()
     {
+        
         DragAndDrop[] dragObjects = FindObjectsOfType<DragAndDrop>();
         SlotHolder[] slotHolders = FindObjectsOfType<SlotHolder>();
         allHoldersFilled = true;
+        bool emptySlotExists = false;
 
         foreach (SlotHolder slotHolder in slotHolders)
         {
             if (!slotHolder.filled)
             {
                 allHoldersFilled = false;
+                emptySlotExists = true;
                 break;
             }
         }
+        
+        int currentPanel = 0; // variable to keep track of current panel
+        int maxPanels = 3; // total number of panels
 
         if (allHoldersFilled)
         {
@@ -66,13 +82,45 @@ public class SlotHolder : MonoBehaviour, IDropHandler
                     {
                         if (dragObject.id == slotHolder.id)
                         {
+                            
                             // Wait until checkButton is clicked before changing color
-                            checkButton.onClick.AddListener(delegate { dragObject.GetComponent<Image>().color = Color.green; });
+                            checkButton.onClick.AddListener(delegate {
+                                
+                                dragObject.GetComponent<Image>().color = Color.green;
+                                
+                                score++;
+                                EasyScoreText.text = score + "/10";
+
+                                if (currentPanel == 1)
+                                {
+                                    Invoke("ActivateMonthPanel1", 1f);
+                                }
+                                else if (currentPanel == 2)
+                                {
+                                    Invoke("ActivateMonthPanel2", 1f);
+                                }
+                                else if (currentPanel == 3)
+                                {
+                                    Invoke("EasyActivateGameOverPanel", 1f);
+                                }
+                                
+                                currentPanel++;
+
+                                // Check if current panel is greater than max panels
+                                if (currentPanel > maxPanels)
+                                {
+                                    currentPanel = maxPanels; // Set current panel to max panels
+                                }
+
+                            });
                         }
                         else
                         {
                             // Wait until checkButton is clicked before changing color
-                            checkButton.onClick.AddListener(delegate { dragObject.GetComponent<Image>().color = Color.red; });
+                            checkButton.onClick.AddListener(delegate {
+                                dragObject.GetComponent<Image>().color = Color.red;
+                                checkButton.onClick.RemoveAllListeners();
+                            });
                         }
                     }
                 }
@@ -82,8 +130,25 @@ public class SlotHolder : MonoBehaviour, IDropHandler
         else
         {
             checkButton.interactable = false;
-            Debug.Log("Not all holders are filled");
+            if (emptySlotExists) Debug.Log("Not all holders are filled");
         }
+
+    }
+
+    void ActivateMonthPanel1(){
+        MonthPanel1.gameObject.SetActive(true);
+        WeekPanel.gameObject.SetActive(false);
+
+    }
+    void ActivateMonthPanel2(){   
+        MonthPanel2.gameObject.SetActive(true);
+        MonthPanel1.gameObject.SetActive(false);
+
+    }
+
+    void EasyActivateGameOverPanel(){
+        GameOverPanel.gameObject.SetActive(true);
+
     }
 }
 
